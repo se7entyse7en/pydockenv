e7b025c215c4f38850fc746ac5bc3f2dcb6e5dfa
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-05-27 11:36:38 +0200

    Add test for deps handling
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- tests/test_pydockenv.py
+++ tests/test_pydockenv.py
@@ -307,3 +307,28 @@ class TestPydockenv(unittest.TestCase):
                 out = self._commander.run('run -- python --version', env=env)
                 self.assertIn(f"Python {d['v']}", out.stdout.decode('utf8'))
                 self._commander.deactivate_env(env=env)
+
+    def test_deps_handling(self):
+        env_name = self._env_name()
+        proj_name, py_version = 'test-proj', '3.7'
+
+        proj_dir = self._create_project_dir(proj_name)
+        self._commander.run(
+            f'create {env_name} {str(proj_dir)} --version={py_version}')
+        with self._commander.active_env(env_name) as env:
+            env['PYTHONPATH'] = definitions.ROOT_DIR
+
+            os.chdir(proj_dir)
+
+            out = self._commander.run('list-packages', env=env)
+            self.assertNotIn(f'pydockenv', out.stdout.decode('utf8'))
+
+            self._commander.run('install pydockenv', env=env)
+
+            out = self._commander.run('list-packages', env=env)
+            self.assertIn(f'pydockenv', out.stdout.decode('utf8'))
+
+            self._commander.run('uninstall -y pydockenv ', env=env)
+
+            out = self._commander.run('list-packages', env=env)
+            self.assertNotIn(f'pydockenv', out.stdout.decode('utf8'))
