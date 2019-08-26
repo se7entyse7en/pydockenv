11846bc28e1caaf99d6fbab9fb787d5c9c2cd41d
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-08-26 11:23:34 +0200

    Expose creation of env from toml file to cli
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- pydockenv/cli.py
+++ pydockenv/cli.py
@@ -12,11 +12,12 @@ def cli():
 
 
 @cli.command()
-@click.argument('name')
 @click.argument('project_dir')
+@click.option('--file', 'file_', help='Toml file')
+@click.option('--name', help='Name of the environment')
 @click.option('--version', help='Python version')
-def create(name, project_dir, version):
-    environment.create(name, project_dir, version)
+def create(project_dir, file_, name, version):
+    environment.create(project_dir, file_, name, version)
 
 
 @cli.command()
