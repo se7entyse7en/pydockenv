7fe083a3d728332f6d61f2df1f560bd1a24ee3f3
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-10-20 18:59:39 +0200

    Update classifiers to include python 3.8
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ classifiers = [
     'Programming Language :: Unix Shell',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Software Development',
     'Topic :: Utilities'
 ]
