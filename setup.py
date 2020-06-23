7a005f6a931198c540baa01569c9951ac8b91d19
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-06-23 10:09:37 +0200

    Make setup.py read metadata from a json file
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-import re
-from functools import partial
+import json
 
 import setuptools
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
@@ -22,28 +21,12 @@ classifiers = [
     'Topic :: Utilities',
 ]
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+with open('README.md', 'r') as fin:
+    long_description = fin.read()
 
 
-def get_about():
-    regexes = {
-        'title': r"^__title__\s=\s(?P<quote>['])(?P<title>\w*)(?P=quote)$",
-        'version': r"^__version__\s=\s(?P<quote>['])(?P<version>[\w\.]*)(?P=quote)$",
-        'author': r"^__author__\s=\s(?P<quote>['])(?P<author>[\w\s]*)(?P=quote)$",
-        'author_email': r"^__author_email__\s=\s(?P<quote>['])(?P<author_email>.*)(?P=quote)$",
-        'description': r"^__description__\s=\s(?P<quote>['])(?P<description>.*)(?P=quote)$",
-        'project_url': r"^__project_url__\s=\s(?P<quote>['])(?P<project_url>.*)(?P=quote)$",
-    }
-
-    with open('./pydockenv/__init__.py') as f:
-        raw_about = f.read()
-
-    extract = partial(re.search, string=raw_about, flags=re.MULTILINE)
-    return {k: extract(v).group(k) for k, v in regexes.items()}
-
-
-about = get_about()
+with open('meta.json') as fin:
+    about = json.load(fin)
 
 
 class bdist_wheel(_bdist_wheel):
